53a174f83e04b05c7187cf52bf384bbf522cc1eb
ted wu <r05543056@g.ntu.edu.tw>
2023-09-15 19:55:17 +0900

    feat:nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
 import useSWR from 'swr'
+import Nav from './components/Nav'
 import {Box} from '@mantine/core'
-import './App.css'
 import { AddTodo } from './components/AddTodo'
 export const ENDPOINT = 'http://localhost:3000'
 
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json())
 
-function App() {
+const  App = () => {
   const {data, isLoading, error} = useSWR('todos', fetcher)
 
   if (error) return <div>error</div>
   if (isLoading) return <div>isLoading</div>
-  return <>
-    <Box>
+  return (
+  <main>
+    <Nav />
+   
+    <Box> 
       {JSON.stringify(data)}
 
       <AddTodo/>
     </Box>
-  </>
+  </main>
+  )
 }
 
 export default App
--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,17 +1,17 @@
 import {useState} from 'react'
-import { useForm } from '@mantine/form'
+// import { useForm } from '@mantine/form'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const style = {
-  position: 'absolute' as 'absolute',
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: 'background.paper',
+  bgcolor: 'background.paper',  
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
@@ -19,20 +19,19 @@ const style = {
 
 export const AddTodo = () => {
     const [open, setOpen] = useState(false)
-    const form = useForm({
-        initialValues:{
-            item:""
-        }
-    })
+    // const form = useForm({
+    //     initialValues:{
+    //         item:""
+    //     }
+    // })
 
     return <>
         <Button onClick={() => setOpen(true)}>Open modal</Button>
         <Modal
-        open={open}
-        onClose = {() => setOpen(false)}
-        
+            open={open}
+            onClose = {() => setOpen(false)}
         >
-            <Box sx={style}>
+           <Box sx={style} >
                 <Typography  variant="h6" component="h2">
                 Text in a modal
                 </Typography>
