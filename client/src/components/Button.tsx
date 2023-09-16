9e93eb47036850c8ba31356d033adf590c5c69ed
ted wu <r05543056@g.ntu.edu.tw>
2023-09-16 16:06:33 +0900

    feat: Bike component left side

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,8 +1,13 @@
 type ButtonProps = {label:string, iconURL: string } 
 export const Button = (props: ButtonProps ) => {
     return (
-        <button>
+        <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg bg-coral-red rounded-full leading-none">
             {props.label}
+
+            <img 
+                src={props.iconURL}
+                alt="arrorRight"
+                className="ml-2 rounded-full w-5 h-5"/>
         </button>
     )
 }
--- client/src/pages/Bikes.tsx
+++ client/src/pages/Bikes.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../components/Button"
 import { arrowRight } from "../assets/icons"
+import { statistics } from "../constants"
 
 
 const Bikes = () => {
@@ -10,16 +11,30 @@ const Bikes = () => {
             justify-center min-h-screen xl:flex-row max-container gap-10"
         >
             <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-                <p>Our Collections</p>
-                <h1>
-                    The New arrivals
+                <p className="text-xl text-coral-red">Our Collections</p>
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New arrivals</span>
                     <br/>
-                    Nicycles
+                    <span className="text-coral-red mt-3 inline-block"> Nicycles </span>
+                    Bikes
                 </h1>
-                <p>Discover with the bikes</p>
-                <Button label="Show Now" iconURL={arrowRight}/>
-            </div>
+                <p className="text-slate-gray text-lg leading-8 mt-6 mb-14 ">Discover stylish the bikes quality comforts  and innovation for your life ksjfkfjioejwvmkmv</p>
+                <Button label="Shop Now" iconURL={arrowRight}/>
+                <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+                    {statistics.map((stats)=>(
+                        <div key={stats.label}>
+                            <p className="text-4xl font-bold font-palanquin">{stats.value}</p>
+                            <p className="leading-7 text-slate-gray font-montserrat">{stats.label}</p>
+                        </div>
+                    )
 
+                    )}
+                </div>
+            </div>
+            {/* for image */}
+        <div>
+
+        </div>
        </section>
     )
 
