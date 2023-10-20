a6aff2954698af9a386c0569a85bd68116fc2d7f
ted wu <r05543056@g.ntu.edu.tw>
2023-10-20 10:21:54 +0900

    feat(UI): add default props to component

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -1,14 +1,17 @@
+import { ComponentProps } from "react"
+import { twMerge } from 'tailwind-merge'
 type ButtonProps = {
 	label: string
 	iconURL?: string
 	borderColor?: string
 	widthFull?: boolean
-}
-export const Button: React.FC<ButtonProps> = ({ label, iconURL, borderColor, widthFull }: ButtonProps) => {
+} & ComponentProps<"button">
+
+export const Button = ({ label, iconURL, borderColor, widthFull, className, ...props }: ButtonProps) => {
 	return (
-		<button className={`flex justify-center items-center gap-2 px-7 py-4 border text-lg rounded-full ${widthFull && 'w-full'} leading-none
-			${borderColor ? `${borderColor}` : 'bg-coral-red'}`
-		}>
+		<button {...props} className={twMerge(`flex justify-center items-center gap-2 px-7 py-4 border text-lg rounded-full ${widthFull && 'w-full'} leading-none
+		${borderColor ? `${borderColor}` : 'bg-coral-red'}`,className)}
+		>
 			{label}
 
 			{iconURL && <img
